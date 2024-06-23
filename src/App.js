96f3ee1135a239aefaae74f8dbbfcaecf5e46b21
BucklewC <bucklewchase@gmail.com>
2024-06-23 09:45:59 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import MyChatBot from './components/mychatbot';
+
+function App() {
+  return (
+    <div className="App">
+      <myChatBot/>
+    </div>
+  );
+}
+
+export default App;
