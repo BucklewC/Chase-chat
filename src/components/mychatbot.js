4beaeba3edae692b9fd12aefa02ace80b0a01eec
BucklewC <bucklewchase@gmail.com>
2024-06-25 20:45:15 -0700

    Update
    Fixed errors, added comments for next steps

--- src/components/mychatbot.js
+++ src/components/mychatbot.js
@@ -35,37 +35,54 @@ const MyChatBot = () => {
         }
       }
     },
-    eligible: {
-      message: "Great, I'll just ask you a few questions to help us get started.",
-      transition: { duration: 1000 },
-      message: "Are you contacting us today about yourself, or someone else?",
-      options: ["Yes", "No"],
+	eligible: {
+		message: "Great, I'll just ask you a few questions to help us get started.",
+		transition: { duration: 1000 },
+		path: "ask_contact_type" 
+	  },
+	  ask_contact_type: { 
+		message: "Are you contacting us today about yourself, or someone else?",
+		function: (params) => setForm({ ...form, contact_type: params.userInput }),
+		options: ["Myself", "My Parent", "My Child", "Another Relative", "Other"],
+		chatDisabled: false,
+		path: (params) => {
+		  if (params.userInput === "Myself") {
+			return "myself";
+		  } else {
+			return "other_relationship";
+		  }
+		}
+	  },
+    myself: {
+      message: "Between September 11, 2001 and May 30, 2002 were you present at any of the following areas?",
+      checkboxes: { items: ["Lower Manhattan Exposure zone Below Canal Street", "Worked with debris at the Fresh Kills landfill", "Pentagon in Washington, D.C.", "Shanksville, PA crash site", "None of the above"], min: 1, max: 5 }, 
       chatDisabled: true,
-      function: (params) => setForm({ ...form, pet_ownership: params.userInput }),
-      path: "ask_choice"
-    },
-    ask_choice: {
-      message: "Select at least 2 and at most 4 pets that you are comfortable to work with:",
-      checkboxes: { items: ["Dog", "Cat", "Rabbit", "Hamster", "Bird"], min: 2, max: 4 },
-      chatDisabled: true,
-      function: (params) => setForm({ ...form, pet_choices: params.userInput }),
-      path: "ask_work_days"
-    },
-    ask_work_days: {
-      message: "How many days can you work per week?",
-      function: (params) => setForm({ ...form, num_work_days: params.userInput }),
-      path: async (params) => {
-        if (isNaN(Number(params.userInput))) {
-          await params.injectMessage("Number of work day(s) need to be a number!");
-          return "ask_work_days";
+      function: (params) => setForm({ ...form, exposure_zone: params.userInput }), 
+      path: (params) => {
+        if (params.userInput === "None of the above") {
+          return "noexposure";
+        } else {
+          return "exposurezone";
         }
-        return "end_success";
       }
     },
-    end_success: {
+    exposurezone: { 
+      message: "Do you have any medical condition such as cancer, respiratory illness, or disorders of the musculoskeletal system?",
+      checkboxes: { items: ["Cancer", "Respiratory illness", "Carpel tunnel syndrome or other disorders of the Musculoskeletal System",  "None of the above"], min: 1, max: 4 },
+      chatDisabled: true,
+      function: (params) => setForm({ ...form, medical_conditions: params.userInput }),
+      path: (params) => {
+        if (params.userInput === "None of the above") {
+          return "nocondition";
+        } else {
+          return "condition";
+        }
+      }
+    },
+    end_success: { 
       message: "We've received your information and will be in contact with you as soon as possible using the contact information you provided. Thank you for contacting Kreindler & Kreindler LLP."
     },
-    end_unqualified: {
+    end_unqualified: { 
       message: "Thank you for contacting Kreindler & Kreindler LLP. If you'd like to get in touch regarding another matter, you can start a new chat."
     }
   };
@@ -76,3 +93,9 @@ const MyChatBot = () => {
 };
 
 export default MyChatBot;
+
+//TO DO: 
+// Finish other_relationship tree + finalization of myself tree 
+//Create Exisiting client (existing) + Something Else (other)
+//Make "none of the above" exclusive -> cannot be selected along with any other option
+//integrate dropdown for disease list
